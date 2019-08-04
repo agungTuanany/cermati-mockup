7b388fdc997adea25ea40640d4b0b941fa334656
agungTuanany <agung.tuanany@gmail.com>
2019-08-04 10:54:22 +0700

    create newsLetter Panel with responsvie media query

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const panelTitle = document.querySelector('.cermati-hl-panel__title');
-const panelIcon = document.querySelector('.cermati-hl-panel__icon');
-const panelDetails = document.querySelector('.cermati-hl-panel__detail');
 const panelsDOM = document.querySelector('.cermati-hl-panel__row');
+const newsLetter = document.querySelector('.cermati-nl-panel');
+const panelBtn = document.querySelector('.cermati-nl-panel-btn__close');
 
 // getting the panel.json
 class Panels {
+
   async getPanels() {
     try  {
       let result = await fetch('panel.json');
@@ -19,11 +19,13 @@ class Panels {
     } catch (err) {
       console.log(err)
     }
-  }
-}
+  };
+
+};
 
 // display single panel
 class UI {
+
   displayPanels(panels) {
     let result = '';
     panels.forEach(panel => {
@@ -37,14 +39,13 @@ class UI {
             <p class='cermati-hl-single-panel__detail'>${panel.details}</p>
           </div>
         </div>
-
         <!-- end of single panel -->
-
       `;
     });
     panelsDOM.innerHTML = result;
-  }
-}
+  };
+
+};
 
 // trigger panels with event
 document.addEventListener('DOMContentLoaded', () => {
@@ -54,5 +55,49 @@ document.addEventListener('DOMContentLoaded', () => {
   // get all panels
   panels.getPanels().then(panels => {
     ui.displayPanels(panels);
-  })
+  });
 });
+
+// scroll newsletter
+function debounce (func, wait = 30, immediate = true) {
+  let timeout;
+  return function()  {
+    let context = this,  args = arguments;
+    let later = () => {
+      timeout = null;
+      if (!immediate) func.aply(context, args);
+    };
+    let callNow = immediate && ! timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if(callNow) func.apply(context, args);
+  };
+};
+
+const checkSlide = (e) => {
+  let windowY = window.scrollY;
+  let innerHeight = window.innerHeight;
+  let newsLetterHeight = newsLetter.scrollHeight;
+  const newsLetterAt = (windowY + innerHeight) - newsLetterHeight / 2;
+  const newsLetterBottom = newsLetter.offsetTop + newsLetterHeight;
+  const isHalfShown = newsLetterAt > newsLetter.offsetTop;
+  const isNotScrollPast = windowY < newsLetterBottom;
+
+  isHalfShown && isNotScrollPast ? newsLetter.classList.add('active') : newsLetter.classList.remove('active');
+
+
+  /* retrieve newsletter position */
+  //console.log(newsLetterHeight)
+  //console.log(`newsLetterAt: ${newsLetterAt}`);
+  //console.log(isHalfShown);
+  //console.log(`newsLetterHeight ${newsLetterHeight}`);
+  //console.log(`newsLetterBottom ${newsLetterBottom}`);
+}
+window.addEventListener('scroll', debounce(checkSlide));
+
+const panelClick = () => {
+  newsLetter.style.display === 'none' ? newsletter.style.display = 'block' : newsLetter.style.transform = 'translateX(-680px)';
+}
+
+panelBtn.addEventListener('click', panelClick);
+
